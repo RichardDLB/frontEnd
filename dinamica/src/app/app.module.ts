1c73243842dd315083011abf85c8596fdc87e414
RichardDLB <richardlopezbo.ar@gmail.com>
2023-02-10 10:17:09 -0300

    generando componentes

--- dinamica/src/app/app.module.ts
+++ dinamica/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { PresentationComponent } from './componentes/presentation/presentation.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { ExperienceComponent } from './componentes/experience/experience.component';
+import { EducationComponent } from './componentes/education/education.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProjectsComponent } from './componentes/projects/projects.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ModalsComponent } from './componentes/modals/modals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PresentationComponent,
+    BannerComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent,
+    ModalsComponent
   ],
   imports: [
     BrowserModule,
