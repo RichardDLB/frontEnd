25a4a393a47bb3354c79e57edd441f2546f9d258
RichardDLB <richardlopezbo.ar@gmail.com>
2023-02-11 09:55:16 -0300

    generando archivo json

--- dinamica/src/app/app-routing.module.ts
+++ dinamica/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { PresentationComponent } from './componentes/presentation/presentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:NavbarComponent},
+  { path: '', component:BannerComponent},  
+  { path: '', component:PresentationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
