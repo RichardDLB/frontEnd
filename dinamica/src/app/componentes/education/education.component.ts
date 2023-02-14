a2fd9dfe5e658bbff54855cb99c6cc6f882d3db5
RichardDLB <richardlopezbo.ar@gmail.com>
2023-02-14 10:18:18 -0300

    puliendo json

--- dinamica/src/app/componentes/education/education.component.ts
+++ dinamica/src/app/componentes/education/education.component.ts
@@ -7,12 +7,20 @@ import { PorfolioService } from 'src/app/servicios/porfolio.service';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+[x: string]: any;
   
+  educationList: any; 
   miPorfolio: any;  
+
 
   constructor(private datosPorfolio:PorfolioService) { }
 
   ngOnInit():void{
+
+    this.datosPorfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.educationList=data.education;
+    });
 
     this.datosPorfolio.obtenerDatos().subscribe(data =>{
       console.log(data);
