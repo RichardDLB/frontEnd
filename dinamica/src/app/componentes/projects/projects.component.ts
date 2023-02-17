2de60727af56810c647084effb4fecf8c20193f4
RichardDLB <richardlopezbo.ar@gmail.com>
2023-02-17 09:42:42 -0300

    terminando maqueta y section projects para enlazar con JSON

--- dinamica/src/app/componentes/projects/projects.component.ts
+++ dinamica/src/app/componentes/projects/projects.component.ts
@@ -16,7 +16,7 @@ export class ProjectsComponent implements OnInit {
 
     this.datosPorfolio.obtenerDatos().subscribe(data =>{
       console.log(data);
-      this.miPorfolio=data;
+      this.miPorfolio=data.projectsList;
     });    
   }
 
